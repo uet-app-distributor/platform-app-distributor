c08452deed1ee0bd1a9ab4a40eb0ecb6ebed698b
Thai Nguyen <thainm.uet@gmail.com>
2023-06-10 21:03:35 +0700

    Update views

--- distributor/views.py
+++ distributor/views.py
@@ -1,10 +1,15 @@
+from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
+
+import json
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
 
+@csrf_exempt
 def generate_app_configuration(request):
-    print(request.body)
+    body = json.loads(request.body)
+    print(body)
     return HttpResponse("Printing request body")
