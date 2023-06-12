4d8a79e8e4f4131bc8a230fea4b1fdc19830807a
Thai Nguyen <thainm.uet@gmail.com>
2023-06-13 00:10:30 +0700

    Add view: distribute

--- distributor/views.py
+++ distributor/views.py
@@ -1,5 +1,34 @@
+from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
+
+import json
 
 
 def index(request):
     return HttpResponse("This is distributor.")
+
+
+def generate_configration_file():
+    pass
+
+
+def upload():
+    pass
+
+
+def trigger_deployment():
+    pass
+
+
+@csrf_exempt
+def distribute(request):
+    # Generate configuration file
+    generate_configration_file()
+
+    # Upload generated file to Cloud Storage
+    upload()
+
+    # Trigger Github Actions deployment workflow
+    trigger_deployment()
+
+    return HttpResponse("Succeed")
