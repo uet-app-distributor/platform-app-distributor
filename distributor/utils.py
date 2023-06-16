d907affebb93af2065a91b186792b740a2830f19
Thai Nguyen <thainm.uet@gmail.com>
2023-06-17 05:00:02 +0700

    Update views

---
+++ distributor/utils.py
@@ -0,0 +1,15 @@
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+
+DEFAULT_TEMPLATE_DIR = './templates'
+class Template:
+    def __init__(self):
+        self.env = Environment(
+            loader=FileSystemLoader(DEFAULT_TEMPLATE_DIR),
+            autoescape=select_autoescape())
+
+    def generate_from_template(self, template, template_vars, file_name):
+        template = self.env.get_template(template)
+        output = template.render(template_vars)
+
+        with open(file_name, "w") as file:
+            file.write(output)
--- distributor/views.py
+++ distributor/views.py
@@ -1,15 +1,39 @@
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
 
+from .utils import Template
+
 import json
+import hashlib
+
+
+DEFAULT_CONFIGURATION_TEMPLATE_FILE = 'configuration.yaml.j2'
 
 
 def index(request):
     return HttpResponse("This is distributor.")
 
 
-def generate_configration_file():
-    pass
+def generate_configration_file(config):
+    def prepare_template_vars():
+        return {
+            'frontend_image': config['frontend']['image'],
+            'frontend_version': config['frontend']['version'],
+            'backend_image': config['backend']['image'],
+            'backend_version': config['backend']['version'],
+            'database_image': config['database']['image']
+        }
+
+    def prepare_output_filename():
+        return hashlib.md5(config['project'].encode())
+
+    template_generator = Template()
+    template_vars = prepare_template_vars()
+    config_filename = prepare_output_filename()
+    template_generator.generate_from_template(DEFAULT_CONFIGURATION_TEMPLATE_FILE,
+                                              template_vars,
+                                              config_filename)
+    return config_filename
 
 
 def upload():
@@ -22,11 +46,14 @@ def trigger_deployment():
 
 @csrf_exempt
 def distribute(request):
+    app_config = json.loads(request.body)
+
     # Generate configuration file
-    generate_configration_file()
+    config_file = generate_configration_file(app_config)
 
     # Upload generated file to Cloud Storage
-    upload()
+    # upload(config_file)
+    print(config_file)
 
     # Trigger Github Actions deployment workflow
     trigger_deployment()
