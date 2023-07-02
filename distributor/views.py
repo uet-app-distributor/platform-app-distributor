5832f697f13a0e93922816aa5983fab9d1a8990c
Thai Nguyen <thainm.uet@gmail.com>
2023-07-02 14:40:42 +0700

    Add upload_to_gcs function

--- distributor/views.py
+++ distributor/views.py
@@ -1,66 +1,57 @@
+import json
+
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
+from google.cloud import storage
 
-from .utils import Template
-
-import json
-import base64
+from .utils import Template, prepare_random_suffix
 
 
 DEFAULT_CONFIGURATION_TEMPLATE_FILE = 'configuration.yaml.j2'
+DEFAULT_GCS_BUCKET = 'uet-app-distributor'
+DEFAULT_GCS_CUSTOMER_APPS = 'customer_apps'
 
 
 def index(request):
     return HttpResponse("This is distributor.")
 
 
-def generate_configration_file(config):
-    def prepare_template_vars():
-        return {
-            'project':                  config['project'],
-            'description':              config['description'],
-            'frontend_image':           config['frontend']['image'],
-            'frontend_image_version':   config['frontend']['version'],
-            'backend_image':            config['backend']['image'],
-            'backend_image_version':    config['backend']['version'],
-            'database_image':           config['database']['image']
-        }
-
-    def prepare_output_filename():
-        project = config['project']
-        project_encode = base64.b64encode(project.encode('ascii'))
-        return f'{project}_{project_encode.decode("ascii")[:8]}'
-
-    template_generator = Template()
-    template_vars = prepare_template_vars()
-    config_filename = prepare_output_filename()
-    print(config_filename)
-    template_generator.generate_from_template(DEFAULT_CONFIGURATION_TEMPLATE_FILE,
-                                              template_vars,
-                                              config_filename)
-    return config_filename
-
-
-def upload():
-    pass
-
-
-def trigger_deployment():
-    pass
-
-
 @csrf_exempt
 def distribute(request):
-    app_config = json.loads(request.body)
+    def generate_config():
+        def prepare_template_vars():
+            return {
+                'project':                  raw_config['project'],
+                'description':              raw_config['description'],
+                'frontend_image':           raw_config['frontend']['image'],
+                'frontend_image_version':   raw_config['frontend']['version'],
+                'backend_image':            raw_config['backend']['image'],
+                'backend_image_version':    raw_config['backend']['version'],
+                'database_image':           raw_config['database']['image']
+            }
 
-    # Generate configuration file
-    config_file = generate_configration_file(app_config)
+        template_generator = Template()
+        template_vars = prepare_template_vars()
+        app_config = template_generator.generate_from_template(DEFAULT_CONFIGURATION_TEMPLATE_FILE, template_vars)
+        return app_config
 
-    # Upload generated file to Cloud Storage
-    # upload(config_file)
-    print(config_file)
+    def upload_to_gcs():
+        storage_client = storage.Client()
+        bucket = storage_client.bucket(DEFAULT_GCS_BUCKET)
+        if bucket.exists():
+            blob_name = f"{DEFAULT_GCS_CUSTOMER_APPS}/{prepare_random_suffix(app_config)}"
+            blob = bucket.blob(blob_name)
+            blob.upload_from_string(app_config, if_generation_match=0)
+            print(f"Blob {blob_name} uploaded to {DEFAULT_GCS_BUCKET}.")
+        else:
+            print(f"Bucket {DEFAULT_GCS_BUCKET}.")
 
-    # Trigger Github Actions deployment workflow
+    def trigger_deployment():
+        print("Triggering deploy...")
+        pass
+
+    raw_config = json.loads(request.body)
+    app_config = generate_config()
+    upload_to_gcs()
     trigger_deployment()
-
     return HttpResponse("Succeed")
