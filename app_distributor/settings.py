4ec71e28aa4890576e367f324bf62f5a81b6f3bb
Thai Nguyen <thainm.uet@gmail.com>
2023-08-18 01:48:42 +0700

    UAD-9 Allow all hosts

--- app_distributor/settings.py
+++ app_distributor/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-64h_in$4!=c29toka&mr)97kb9l6+0nm0s-nfy1bdc-$=fhnwt
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
